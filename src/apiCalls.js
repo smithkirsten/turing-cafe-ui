fbea89205082b55e218b27180b0346f715df8cc2
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 12:21:15 -0500

    Add fetch call to getReservations

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,14 @@
-const getReservations = () => {
-
+const getReservations = async() => {
+  url = 'http://localhost:3001/api/v1/reservations'
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  }
+  catch {
+    console.log(error)
+    return error
+  }
 }
 
 export { getReservations }
