dd0269c63aacf2b31a5c91ddb24e39bae4b58fb0
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 13:37:44 -0500

    Add Cypress tests for content on load

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -4,6 +4,15 @@ describe('Landing page', () => {
     cy.visit('http://localhost:3000/')
   })
   it('Has a title', () => {
-    cy.get('')
+    cy.get('.app-title').contains('Turing Cafe Reservations')
+  })
+  it('Has a form', () => {
+    cy.get('input').should('have.length', 4)
+    cy.get('.submit-button').should('exist')
+  })
+  it('should display reservations', () => {
+    cy.get('.res-card').should('have.length', 3)
+    cy.get('#1').contains('Christie')
+    cy.get('#3').contains('Lars')
   })
 })
