946bf8dc1ddef13696b11b94450850dc4e573f46
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 12:29:25 -0500

    Fix error catch syntax in apiCall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
 const getReservations = async() => {
-  url = 'http://localhost:3001/api/v1/reservations'
+  const url = 'http://localhost:3001/api/v1/reservations'
   try {
     const response = await fetch(url)
     const data = await response.json()
     return data
   }
-  catch {
+  catch(error) {
     console.log(error)
     return error
   }
