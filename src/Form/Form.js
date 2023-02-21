2b67016c38e4e8146eb39b20e112796dbd4e4834
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 12:01:12 -0500

    Add value and onChange to inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,13 +11,16 @@ class Form extends Component {
       guests: ''
     }
   }
+  handleChange = () => {
+    
+  }
   //time and date are types of inputs
   render() {
     return (<form>
-      <input name='name' type='text' />
-      <input name='date' type='text' />
-      <input name='time' type='text' />
-      <input name='guests' type='number' />
+      <input name='name' value={this.state.name} onChange={(event) => this.handleChange(event.target)}type='text' />
+      <input name='date' value={this.state.date} onChange={(event) => this.handleChange(event.target)} type='text' />
+      <input name='time' value={this.state.time} onChange={(event) => this.handleChange(event.target)} type='text' />
+      <input name='guests' value={this.state.guests} onChange={(event) => this.handleChange(event.target)} type='number' />
     </form>)
   }
 }
