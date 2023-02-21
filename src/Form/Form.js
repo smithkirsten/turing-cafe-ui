28834ef155272943e3577d0cb7abd997357475f8
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 12:41:43 -0500

    Change guest property to number

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -8,7 +8,7 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      guests: ''
+      number: ''
     }
   }
   handleChange = (target) => {
@@ -28,8 +28,8 @@ class Form extends Component {
         <input name='date' value={this.state.date} onChange={(event) => this.handleChange(event.target)} placeholder="date(mm/dd)" type='text' />
       <label htmlFor="time" className="hidden">Time</label>
         <input name='time' value={this.state.time} onChange={(event) => this.handleChange(event.target)} placeholder="time" type='text' />
-      <label htmlFor='guests' className="hidden">Number of Guests</label>
-        <input name='guests' value={this.state.guests} onChange={(event) => this.handleChange(event.target)} placeholder="number of guests" type='number' />
+      <label htmlFor='number' className="hidden">Number of Guests</label>
+        <input name='number' value={this.state.number} onChange={(event) => this.handleChange(event.target)} placeholder="number of guests" type='number' />
       <button className='submit-button' type='submit' onClick={this.handleClick}>Make Reservation</button> 
     </form>)
   }
