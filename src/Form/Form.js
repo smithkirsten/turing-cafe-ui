4e5e175508e99d0634a3054426f23d52403e8fdc
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-21 12:32:05 -0500

    Fix for -> htmlFor in labels

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -22,13 +22,13 @@ class Form extends Component {
   //time and date are types of inputs
   render() {
     return (<form>
-      <label for="name" className="hidden">Name</label>
+      <label htmlFor="name" className="hidden">Name</label>
         <input name='name' value={this.state.name} onChange={(event) => this.handleChange(event.target)} placeholder="name" type='text' />
-      <label for="date" className="hidden">Date</label>
+      <label htmlFor="date" className="hidden">Date</label>
         <input name='date' value={this.state.date} onChange={(event) => this.handleChange(event.target)} placeholder="date(mm/dd)" type='text' />
-      <label for="time" className="hidden">Time</label>
+      <label htmlFor="time" className="hidden">Time</label>
         <input name='time' value={this.state.time} onChange={(event) => this.handleChange(event.target)} placeholder="time" type='text' />
-      <label for='guests' className="hidden">Number of Guests</label>
+      <label htmlFor='guests' className="hidden">Number of Guests</label>
         <input name='guests' value={this.state.guests} onChange={(event) => this.handleChange(event.target)} placeholder="number of guests" type='number' />
       <button className='submit-button' type='submit' onClick={this.handleClick}>Make Reservation</button> 
     </form>)
